294341b6f48efb804a7e62ab63fb12208f7bb169
chandhinikonagalla <69637175+chandhinikonagalla@users.noreply.github.com>
2020-09-14 18:18:44 +0530

    Add files via upload

---
+++ collec1.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class ArrayListLoopExample {
+    public static void main(String args[]) {
+        // initialize ArrayList  
+        
+		int n,temp,sum=0,t2=0,index=1;
+		Scanner sc = new Scanner(System.in);
+		
+		n=sc.nextInt();
+ArrayList<Integer> al = new ArrayList<Integer>();
+al.add(0);
+		for(int i=1;i<=n;i++){
+			temp=sc.nextInt();
+			al.add(temp);
+			//index+=1;
+		}
+      
+		
+		for(int i=1;i<al.size();i++){
+			t2=al.get(i);
+			
+			if(i%2==0){
+				if(t2%2!=0)
+					sum+=al.get(i);
+			}
+			
+			if(i%2!=0){
+				if(t2%2==0)
+					sum+=al.get(i);
+			}
+		}
+        System.out.println(sum);   
+        
+}}
